4d9ab2d41631f608231a198e6043822cd03fcccb
Radai <lordradai@null.net>
2023-09-01 00:20:59 +0200

    TimeAct mapping update, NMB generation fix
    Fixed issues with NMB generation.
    Fixed incorrect TimeAct layout for event 260005 which caused the player character to turn invisible while dodging

---
+++ src/MorphemeConnect/FromsoftFormat/TimeActReader/TimeActEvent/TimeActEvent.cpp
@@ -0,0 +1,413 @@
+#include "TimeActEvent.h"
+
+TimeActEventData* getArguments(int id)
+{
+	switch (id)
+	{
+	case 100:
+		return new TimeActEventData_NoArgs;
+	case 201:
+		return new TimeActEventData_Sound201;
+	case 202:
+		return new TimeActEventData_Sound202;
+	case 300:
+		return new TimeActEventData_Ai300;
+	case 301:
+		return new TimeActEventData_Ai300;
+	case 1220:
+		return new TimeActEventData_NoArgs;
+	case 1230:
+		return new TimeActEventData_NoArgs;
+	case 1500:
+		return new TimeActEventData_Camera1500;
+	case 1501:
+		return new TimeActEventData_Camera1501;
+	case 1502:
+		return new TimeActEventData_Camera1502;
+	case 1503:
+		return new TimeActEventData_Camera1503;
+	case 1504:
+		return new TimeActEventData_Camera1504;
+	case 2100:
+		return new TimeActEventData_NoArgs;
+	case 2101:
+		return new TimeActEventData_NoArgs;
+	case 2102:
+		return new TimeActEventData_NoArgs;
+	case 2103:
+		return new TimeActEventData_Sfx2103;
+	case 2104:
+		return new TimeActEventData_Sfx2104;
+	case 2105:
+		return new TimeActEventData_NoArgs;
+	case 2106:
+		return new TimeActEventData_Sfx2106;
+	case 2200:
+		return new TimeActEventData_Damage2200;
+	case 2201:
+		return new TimeActEventData_Damage2201;
+	case 2202:
+		return new TimeActEventData_Damage2202;
+	case 2203:
+		return new TimeActEventData_Damage2203;
+	case 2300:
+		return new TimeActEventData_Bullet2300;
+	case 2301:
+		return new TimeActEventData_Bullet2301;
+	case 2400:
+		return new TimeActEventData_NoArgs;
+	case 2401:
+		return new TimeActEventData_NoArgs;
+	case 2402:
+		return new TimeActEventData_FootEffect2402;
+	case 2403:
+		return new TimeActEventData_NoArgs;
+	case 2500:
+		return new TimeActEventData_Weapon2500;
+	case 2501:
+		return new TimeActEventData_Weapon2501;
+	case 2502:
+		return new TimeActEventData_NoArgs;
+	case 2600:
+		return new TimeActEventData_NoArgs;
+	case 101100:
+		return new TimeActEventData_NoArgs;
+	case 110000:
+		return new TimeActEventData_NoArgs;
+	case 110100:
+		return new TimeActEventData_NoArgs;
+	case 110200:
+		return new TimeActEventData_NoArgs;
+	case 110210:
+		return new TimeActEventData_NoArgs;
+	case 110300:
+		return new TimeActEventData_NoArgs;
+	case 110400:
+		return new TimeActEventData_NoArgs;
+	case 110500:
+		return new TimeActEventData_NoArgs;
+	case 110600:
+		return new TimeActEventData_NoArgs;
+	case 110700:
+		return new TimeActEventData_NoArgs;
+	case 110800:
+		return new TimeActEventData_NoArgs;
+	case 110900:
+		return new TimeActEventData_NoArgs;
+	case 111000:
+		return new TimeActEventData_DamageAction111000;
+	case 111100:
+		return new TimeActEventData_NoArgs;
+	case 111200:
+		return new TimeActEventData_NoArgs;
+	case 111300:
+		return new TimeActEventData_NoArgs;
+	case 111400:
+		return new TimeActEventData_NoArgs;
+	case 111500:
+		return new TimeActEventData_NoArgs;
+	case 111600:
+		return new TimeActEventData_NoArgs;
+	case 111700:
+		return new TimeActEventData_NoArgs;
+	case 111800:
+		return new TimeActEventData_NoArgs;
+	case 111900:
+		return new TimeActEventData_NoArgs;
+	case 120100:
+		return new TimeActEventData_NoArgs;
+	case 120200:
+		return new TimeActEventData_NoArgs;
+	case 120300:
+		return new TimeActEventData_NoArgs;
+	case 210000:
+		return new TimeActEventData_NoArgs;
+	case 210099:
+		return new TimeActEventData_NoArgs;
+	case 210100:
+		return new TimeActEventData_NoArgs;
+	case 210200:
+		return new TimeActEventData_NoArgs;
+	case 220000:
+		return new TimeActEventData_NoArgs;
+	case 220100:
+		return new TimeActEventData_NoArgs;
+	case 220200:
+		return new TimeActEventData_Item220200;
+	case 220300:
+		return new TimeActEventData_NoArgs;
+	case 230000:
+		return new TimeActEventData_NoArgs;
+	case 230001:
+		return new TimeActEventData_NoArgs;
+	case 240000:
+		return new TimeActEventData_NoArgs;
+	case 250000:
+		return new TimeActEventData_NoArgs;
+	case 260000:
+		return new TimeActEventData_NoArgs;
+	case 260001:
+		return new TimeActEventData_Model260001;
+	case 260002:
+		return new TimeActEventData_Model260002;
+	case 260003:
+		return new TimeActEventData_NoArgs;
+	case 260004:
+		return new TimeActEventData_Model260004;
+	case 260005:
+		return new TimeActEventData_Model260005;
+	case 260006:
+		return new TimeActEventData_Model260006;
+	case 270000:
+		return new TimeActEventData_Model270000;
+	case 270001:
+		return new TimeActEventData_NoArgs;
+	case 270002:
+		return new TimeActEventData_NoArgs;
+	case 280000:
+		return new TimeActEventData_NoArgs;
+	case 280100:
+		return new TimeActEventData_Move280100;
+	case 290000:
+		return new TimeActEventData_NoArgs;
+	case 290001:
+		return new TimeActEventData_NoArgs;
+	case 290002:
+		return new TimeActEventData_NoArgs;
+	case 300000:
+		return new TimeActEventData_Event300000;
+	case 300100:
+		return new TimeActEventData_Event300100;
+	case 300200:
+		return new TimeActEventData_Event300200;
+	case 300300:
+		return new TimeActEventData_Event300300;
+	case 310000:
+		return new TimeActEventData_SpEffect310000;
+	case 500000:
+		return new TimeActEventData_MapGimmick500000;
+	case 502000:
+		return new TimeActEventData_MapSound502000;
+	default:
+		return new TimeActEventData_NoArgs;
+	}
+}
+
+TimeActEvent::EventData::EventData() 
+{
+	this->m_id = 0;
+	this->m_pad = 0;
+	this->m_args = getArguments(-1);
+}
+
+TimeActEvent::EventData::EventData(int id)
+{
+	this->m_id = id;
+
+	this->m_args = getArguments(id);
+}
+
+TimeActEvent::EventData::EventData(ifstream* tae)
+{
+	MemReader::ReadDWord(tae, (DWORD*)&this->m_id);
+	MemReader::ReadDWord(tae, (DWORD*)&this->m_pad);
+	MemReader::ReadQWord(tae, &this->m_argsOffset);
+
+	tae->seekg(m_argsOffset);
+
+	this->m_args = getArguments(this->m_id);
+
+	this->m_args->GetData(tae);
+}
+
+TimeActEvent::EventData::~EventData()
+{
+}
+
+TimeActEvent::TimeActEvent()
+{
+	this->m_start = 0;
+	this->m_end = 0;
+	this->m_eventData = new EventData();
+}
+
+TimeActEvent::TimeActEvent(float start, float end, int id)
+{
+	this->m_start = start;
+	this->m_end = end;
+	this->m_eventData = new EventData(id);
+}
+
+TimeActEvent::TimeActEvent(ifstream* tae)
+{
+	streampos start = tae->tellg();
+
+	MemReader::ReadQWord(tae, &this->m_startOffset);
+	MemReader::ReadQWord(tae, &this->m_endOffset);
+	MemReader::ReadQWord(tae, &this->m_eventDataOffset);
+
+	if (m_startOffset)
+	{
+		tae->seekg(m_startOffset);
+
+		MemReader::ReadDWord(tae, (DWORD*)&this->m_start);
+	}
+
+	if (m_endOffset)
+	{
+		tae->seekg(m_endOffset);
+
+		MemReader::ReadDWord(tae, (DWORD*)&this->m_end);
+	}
+
+	if (m_eventDataOffset)
+	{
+		tae->seekg(m_eventDataOffset);
+
+		this->m_eventData = new EventData(tae);
+	}
+
+	tae->seekg(start + (streampos)0x18);
+}
+
+TimeActEvent::~TimeActEvent()
+{
+}
+
+void TimeActEvent::GenerateBinary(ofstream* tae)
+{
+	MemReader::WriteQWord(tae, &this->m_startOffset);
+	MemReader::WriteQWord(tae, &this->m_endOffset);
+	MemReader::WriteQWord(tae, &this->m_eventDataOffset);
+
+	UINT64 bak = tae->tellp();
+	tae->seekp(m_eventDataOffset);
+
+	MemReader::WriteDWord(tae, (DWORD*)&this->m_eventData->m_id);
+	MemReader::WriteDWord(tae, (DWORD*)&this->m_eventData->m_pad);
+
+	MemReader::WriteQWord(tae, &this->m_eventData->m_argsOffset);
+
+	tae->seekp(this->m_eventData->m_argsOffset);
+
+	this->m_eventData->m_args->SaveData(tae);
+
+	tae->seekp(bak);
+}
+
+int TimeActEvent::GetArgumentsSize()
+{
+	int size = 64;
+
+	int remainder = size % 16;
+
+	if (remainder != 0)
+		size += 16 - remainder;
+
+	return size + 0x18;
+}
+
+EventGroup::EventGroupData::EventGroupData() {}
+
+EventGroup::EventGroupData::EventGroupData(ifstream* tae)
+{
+	streampos bak = tae->tellg();
+
+	MemReader::ReadQWord(tae, &this->m_eventType);
+	MemReader::ReadQWord(tae, &this->m_offset);
+}
+
+EventGroup::EventGroup() 
+{
+	this->m_count = 0;
+	this->m_pad = 0;
+}
+
+EventGroup::EventGroup(int id)
+{
+	this->m_count = 0;
+	this->m_pad = 0;
+
+	this->m_groupData = new EventGroupData;
+
+	this->m_groupData->m_eventType = id;
+}
+
+EventGroup::EventGroup(ifstream* tae, UINT64 eventStartOffset)
+{
+	streampos bak = tae->tellg();
+
+	MemReader::ReadQWord(tae, &this->m_count);
+	MemReader::ReadQWord(tae, &this->m_eventsOffset);
+	MemReader::ReadQWord(tae, &this->m_groupDataOffset);
+	MemReader::ReadQWord(tae, &this->m_pad);
+
+	if (this->m_count > 0)
+	{
+		if (m_eventsOffset)
+		{
+			UINT64 offset = m_eventsOffset;
+
+			for (size_t i = 0; i < this->m_count; i++)
+			{
+				tae->seekg(offset);
+
+				UINT64 event_offset;
+				MemReader::ReadQWord(tae, &event_offset);
+				this->m_eventOffset.push_back(event_offset);
+
+				int idx = (event_offset - eventStartOffset) / 0x18;
+
+				this->m_eventIndex.push_back(idx);
+
+				tae->seekg(event_offset);
+
+				offset += 0x8;
+			}
+		}
+	}
+
+	if (m_groupDataOffset)
+	{
+		tae->seekg(m_groupDataOffset);
+
+		this->m_groupData = new EventGroupData(tae);
+	}
+
+	bak += 0x20;
+	tae->seekg(bak);
+}
+
+EventGroup::~EventGroup()
+{
+}
+
+void EventGroup::GenerateBinary(ofstream* tae)
+{
+	MemReader::WriteQWord(tae, &this->m_count);
+	MemReader::WriteQWord(tae, &this->m_eventsOffset);
+	MemReader::WriteQWord(tae, &this->m_groupDataOffset);
+
+	UINT64 pad0 = 0;
+	MemReader::WriteQWord(tae, &pad0);
+
+	UINT64 bak = tae->tellp();
+
+	if (this->m_count)
+	{
+		tae->seekp(m_groupDataOffset);
+
+		MemReader::WriteQWord(tae, &this->m_groupData->m_eventType);
+		MemReader::WriteQWord(tae, &this->m_groupData->m_offset);
+
+		UINT64 pad0[2] = { 0, 0 };
+		MemReader::WriteQWordArray(tae, pad0, 2);
+
+		tae->seekp(m_eventsOffset);
+
+		for (size_t i = 0; i < this->m_count; i++)
+			MemReader::WriteQWord(tae, &this->m_eventOffset[i]);
+	}
+
+	tae->seekp(bak);
+}
---
+++ src/MorphemeConnect/FromsoftFormat/TimeActReader/TimeActEvent/TimeActEventData/TimeActEventData.h
@@ -0,0 +1,485 @@
+#include <fstream>
+#include <Windows.h>
+#pragma once
+
+using namespace std;
+
+class TimeActEventData
+{
+public:
+	int m_size;
+
+	virtual void GetData(ifstream* tae) { return; }
+	virtual void SaveData(ofstream* tae) { return; }
+	virtual std::string GetArgumentAsString() { return ""; }
+	virtual void ImGuiEditSection() { return; }
+};
+
+class TimeActEventData_NoArgs : public TimeActEventData
+{
+public:
+	BYTE* m_data;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Sound201 : public TimeActEventData
+{
+public:
+	int m_soundId;
+	int m_iVar4;
+	short m_sVar8;
+	short m_sVarA;
+	short m_sVarC;
+	short m_sVarE;
+	short m_sVar10;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Sound202 : public TimeActEventData
+{
+public:
+	int m_soundId;
+	int m_iVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Ai300 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Ai301 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+	int m_iVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Damage2200 : public TimeActEventData
+{
+public:
+	int m_index;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Damage2201 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Damage2202: public TimeActEventData
+{
+public:
+	int m_damageParamId;
+	int m_iVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Damage2203 : public TimeActEventData
+{
+public:
+	int m_damageParamId;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Bullet2300 : public TimeActEventData
+{
+public:
+	int m_index;
+	int m_iVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Bullet2301 : public TimeActEventData
+{
+public:
+	int m_bulletParamId;
+	int m_damageParamId;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Camera1500 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Camera1501 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Camera1502 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Camera1503 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Camera1504 : public TimeActEventData
+{
+public:
+	int m_cameraExFollowImpactParamId;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Sfx2103 : public TimeActEventData
+{
+public:
+	int m_chrFullBodySfxId;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Sfx2104 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+	int m_sfxId;
+	int m_dmyPolyId;
+	BYTE m_bVarC;
+	BYTE m_bVarD;
+	BYTE m_bVarE;
+	BYTE m_bVarF;
+	int m_iVar10;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Sfx2106 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+	int m_sfxId;
+	int m_dmyPolyId;
+	BYTE m_bVarC;
+	BYTE m_bVarD;
+	BYTE m_bVarE;
+	BYTE m_bVarF;
+	int m_iVar10;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_FootEffect2402 : public TimeActEventData
+{
+public:
+	int m_id;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Weapon2500 : public TimeActEventData
+{
+public:
+	short m_sVar0;
+	short m_sVar2;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Weapon2501 : public TimeActEventData
+{
+public:
+	short m_sVar0;
+	short m_sVar2;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_DamageAction111000 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+	int m_iVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Item220200 : public TimeActEventData
+{
+public:
+	int m_type;
+	int m_index;
+	int m_iVar8;
+	int m_iVarC;
+	float m_fVar10;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Model260001 : public TimeActEventData
+{
+public:
+	int m_type;
+	int m_iVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Model260002 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+	int m_iVar4;
+	int m_iVar8;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Model260004 : public TimeActEventData
+{
+public:
+	float m_fVar0;
+	float m_fVar4;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Model260005 : public TimeActEventData
+{
+public:
+	float m_fVar0;
+	float m_fVar4;
+	BYTE m_bVar5;
+	BYTE m_condition;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Model260006 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Model270000 : public TimeActEventData
+{
+public:
+	int m_partsId;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Move280100 : public TimeActEventData
+{
+public:
+	float m_turnRate;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Event300000 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Event300100 : public TimeActEventData
+{
+public:
+	int m_dmyPolyId;
+	int m_padRumbleParamId;
+	float m_intensity;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Event300200 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_Event300300 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_SpEffect310000 : public TimeActEventData
+{
+public:
+	int m_spEffectId;
+	short m_type;
+	short m_mode;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_MapGimmick500000 : public TimeActEventData
+{
+public:
+	int m_iVar0;
+	int m_iVar4;
+	int m_iVar8;
+	int m_iVarC;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};
+
+class TimeActEventData_MapSound502000 : public TimeActEventData
+{
+public:
+	int m_soundId;
+	BYTE m_bVar4;
+	BYTE m_bVar5;
+	short m_sVar6;
+	int m_iVar8;
+	int m_iVarC;
+
+	void GetData(ifstream* tae);
+	void SaveData(ofstream* tae);
+	std::string GetArgumentAsString();
+	void ImGuiEditSection();
+};

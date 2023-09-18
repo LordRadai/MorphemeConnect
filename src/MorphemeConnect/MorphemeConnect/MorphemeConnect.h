626e820c864f88576bf772cca5af2b5f53870d43
Radai <lordradai@null.net>
2023-09-18 19:33:35 +0200

    Add files via upload

--- src/MorphemeConnect/MorphemeConnect/MorphemeConnect.h
+++ src/MorphemeConnect/MorphemeConnect/MorphemeConnect.h
@@ -1,6 +1,6 @@
 #pragma once
-#define APPNAME_W L"MorphemeConnect 1.0.45.2"
-#define APPNAME_A "MorphemeConnect 1.0.45.2"
+#define APPNAME_W L"MorphemeConnect 1.0.45.3"
+#define APPNAME_A "MorphemeConnect 1.0.45.3"
 
 #include "resource.h"
 #include <d3d11.h>

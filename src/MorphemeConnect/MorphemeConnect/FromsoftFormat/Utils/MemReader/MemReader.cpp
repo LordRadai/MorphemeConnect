3370eb236e4a43fcf90c6e670539a7d07b2e5c84
Radai <lordradai@null.net>
2024-05-04 16:30:28 +0200

    Update MemReader.cpp

--- src/MorphemeConnect/MorphemeConnect/FromsoftFormat/Utils/MemReader/MemReader.cpp
+++ src/MorphemeConnect/MorphemeConnect/FromsoftFormat/Utils/MemReader/MemReader.cpp
@@ -17,7 +17,7 @@ template <typename T> void MemReader::Write(ofstream* pStream, T v)
 
 template <typename T> void MemReader::WriteArray(ofstream* pStream, const T* pVal, size_t iCount)
 {
-	pStream->write(pVal, iCount * sizeof(T));
+	pStream->write((const char*)pVal, iCount * sizeof(T));
 }
 
 void MemReader::Pad(ofstream* pStream, BYTE padVal, int iCount)

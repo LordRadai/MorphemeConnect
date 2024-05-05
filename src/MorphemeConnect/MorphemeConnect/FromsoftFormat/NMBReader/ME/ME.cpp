eb3a96edc05958f721810e58d2f311ae9e6b388d
Radai <lordradai@null.net>
2024-05-05 10:47:09 +0200

    rework

--- src/MorphemeConnect/MorphemeConnect/FromsoftFormat/NMBReader/ME/ME.cpp
+++ src/MorphemeConnect/MorphemeConnect/FromsoftFormat/NMBReader/ME/ME.cpp
@@ -15,40 +15,40 @@ void ME::NodeExportXML(XMLElement* pRoot, std::string name, UINT network_id, UIN
 		pRoot->SetAttribute("downstreamMultiplyConnected", "true");
 }
 
-void ME::TakeListXML(tinyxml2::XMLDocument* pDoc, std::string name)
+void ME::TakeListXML(tinyxml2::XMLDocument* pDoc, std::string name, int version)
 {
 	tinyxml2::XMLElement* pRoot = pDoc->NewElement("TakeList");
 	pRoot->SetAttribute("sourceAnimFile", name.c_str());
-	pRoot->SetAttribute("version", 1);
+	pRoot->SetAttribute("version", version);
 }
 
-void ME::TakeExportXML(XMLElement* pRoot, std::string name, NodeAttribBool* is_loop, NodeAttribSourceAnim* source_anim, NodeAttribSourceEventTrack* event_track)
+void ME::TakeExportXML(XMLElement* pRoot, std::string name, bool is_loop, float duration, float fps, float clipStart, float clipEnd)
 {
 	XMLElement* take = pRoot->InsertNewChildElement("Take");
 	take->SetAttribute("name", name.c_str());
 	
 	XMLElement* attributes = take->InsertNewChildElement("Attributes");
 
-	XMLElement* duration = attributes->InsertNewChildElement("secondsDuration");
-	duration->SetAttribute("type", "double");
-	duration->SetText(source_anim->m_animLen);
+	XMLElement* pDuration = attributes->InsertNewChildElement("secondsDuration");
+	pDuration->SetAttribute("type", "double");
+	pDuration->SetText(duration);
 
-	XMLElement* fps = attributes->InsertNewChildElement("fps");
-	fps->SetAttribute("type", "double");
-	fps->SetText(30);
+	XMLElement* pFps = attributes->InsertNewChildElement("fps");
+	pFps->SetAttribute("type", "double");
+	pFps->SetText(30);
 
-	XMLElement* clipStart = attributes->InsertNewChildElement("clipStart");
-	clipStart->SetAttribute("type", "double");
-	clipStart->SetText(source_anim->m_clipStart);
+	XMLElement* pClipStart = attributes->InsertNewChildElement("clipStart");
+	pClipStart->SetAttribute("type", "double");
+	pClipStart->SetText(clipStart);
 
-	XMLElement* clipStart = attributes->InsertNewChildElement("clipEnd");
-	clipStart->SetAttribute("type", "double");
-	clipStart->SetText(source_anim->m_clipEnd);
+	XMLElement* pClipEnd = attributes->InsertNewChildElement("clipEnd");
+	pClipEnd->SetAttribute("type", "double");
+	pClipEnd->SetText(clipEnd);
 
 	XMLElement* loop = attributes->InsertNewChildElement("loop");
 	loop->SetAttribute("type", "bool");
 
-	if (is_loop->m_bool)
+	if (is_loop)
 		loop->SetText("true");
 	else
 		loop->SetText("false");
@@ -136,9 +136,6 @@ void ME::DurationEventTrackExportXML(XMLElement* pRoot, MorphemeBundle_EventTrac
 	XMLElement* userData = pRoot->InsertNewChildElement("userData");
 	userData->SetAttribute("type", "uint");
 	userData->SetText(event_track->m_data->m_eventId);
-
-	for (size_t i = 0; i < event_track->m_data->m_numEvents; i++)
-		ME::DurationEventExportXML(discreteEventTrack, i, event_track->m_data->m_events[i].m_value, event_track->m_data->m_events[i].m_start, event_track->m_data->m_events[i].m_duration);
 }
 
 void ME::DurationEventExportXML(XMLElement* pRoot, int event_idx, int user_data, float start_time, float end_time)
--- src/MorphemeConnect/MorphemeConnect/FromsoftFormat/NMBReader/ME/ME.h
+++ src/MorphemeConnect/MorphemeConnect/FromsoftFormat/NMBReader/ME/ME.h
@@ -9,8 +9,8 @@ namespace ME
 {
 	void NodeExportXML(XMLElement* pRoot, std::string name, UINT network_id, UINT parent_id, UINT type_id, bool persistent, bool downstream_multiply_connected);
 	
-	void TakeListXML(tinyxml2::XMLDocument* pDoc, std::string name);
-	void TakeExportXML(XMLElement* pRoot, std::string name, NodeAttribBool* is_loop, NodeAttribSourceAnim* source_anim, NodeAttribSourceEventTrack* event_track);
+	void TakeListXML(tinyxml2::XMLDocument* pDoc, std::string name, int version);
+	void TakeExportXML(XMLElement* pRoot, std::string name, bool is_loop, float duration, float fps, float clipStart, float clipEnd);
 	void DiscreteEventTrackExportXML(XMLElement* pRoot, MorphemeBundle_EventTrack* event_track);
 	void DiscreteEventExportXML(XMLElement* pRoot, int event_idx, int user_data, float start_time);
 	void CurveEventTrackExportXML(XMLElement* pRoot, MorphemeBundle_EventTrack* event_track);

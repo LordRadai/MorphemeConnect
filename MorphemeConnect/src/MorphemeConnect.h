3b3a846df60e90d38edf3deec19befbcbda488c6
Radai <lordradai@null.net>
2024-05-17 14:29:49 +0200

    Update MorphemeConnect.h

--- MorphemeConnect/src/MorphemeConnect.h
+++ MorphemeConnect/src/MorphemeConnect.h
@@ -1,6 +1,6 @@
 #pragma once
-#define APPNAME_W L"MorphemeConnect 1.1.0.0"
-#define APPNAME_A "MorphemeConnect 1.1.0.0"
+#define APPNAME_W L"MorphemeConnect 1.1.1.0"
+#define APPNAME_A "MorphemeConnect 1.1.1.0"
 
 #include "resource.h"
 #include <d3d11.h>

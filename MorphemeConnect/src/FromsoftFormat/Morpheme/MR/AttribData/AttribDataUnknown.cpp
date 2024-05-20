8fd2c8f468b6e3656c570298df35764ad2332bb9
Radai <lordradai@null.net>
2024-05-20 14:09:14 +0200

    Update AttribDataUnknown.cpp

--- MorphemeConnect/src/FromsoftFormat/Morpheme/MR/AttribData/AttribDataUnknown.cpp
+++ MorphemeConnect/src/FromsoftFormat/Morpheme/MR/AttribData/AttribDataUnknown.cpp
@@ -8,8 +8,10 @@ AttribDataUnknown::AttribDataUnknown()
 
 AttribDataUnknown::AttribDataUnknown(BYTE* pData, int size) : AttribData(pData)
 {
-	this->m_data.reserve(size);
-	for (size_t i = 0; i < size; i++)
+	pData += 0x10;
+
+	this->m_data.reserve(size - 16);
+	for (size_t i = 0; i < size - 16; i++)
 		this->m_data.push_back(pData[i]);
 }
 

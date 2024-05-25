3d9940f30c9e2b9fe6bd132f876fd62ab420fe85
Radai <lordradai@null.net>
2024-05-25 21:25:37 +0200

    res

--- MorphemeConnect/morphemeEditor/Resource.h
+++ MorphemeConnect/morphemeEditor/Resource.h
@@ -1,30 +1,17 @@
 //{{NO_DEPENDENCIES}}
-// File di inclusione generato da Microsoft Visual C++.
-// Usato da MorphemeConnect.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_MORPHEMECONNECT_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_MORPHEMECONNECT			107
-#define IDI_SMALL				108
-#define IDC_MORPHEMECONNECT			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Valori predefiniti successivi per i nuovi oggetti
+// Microsoft Visual C++ generated include file.
+// Used by morphemeEditor.rc
 //
+#define IDI_ICON1                       101
+#define IDR_ACCELERATOR1                102
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

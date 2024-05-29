9be13c5e2b5be11399d2027436cbf6dba07c1141
Radai <lordradai@null.net>
2024-05-29 19:12:40 +0200

    Update FlverModel.cpp

--- MorphemeConnect/morphemeEditor/FlverModel/FlverModel.cpp
+++ MorphemeConnect/morphemeEditor/FlverModel/FlverModel.cpp
@@ -485,7 +485,7 @@ void FlverModel::UpdateModel()
 		for (size_t j = 0; j < this->m_vertBindPose[i].size(); j++)
 			m_vertBindPose[i][j].m_pos.color = color;
 
-	this->m_focusPoint = this->m_position;
+	//this->m_focusPoint = this->m_position;
 }
 
 int FlverModel::GetBoneIndexFromName(const char* name)
@@ -541,7 +541,7 @@ void FlverModel::Animate(MR::AnimationSourceHandle* animHandle, std::vector<int>
 		this->m_morphemeBoneTransforms.push_back(ComputeNmBoneGlobalTransform(animHandle, i));
 	}
 
-	this->m_position = Vector3::Transform(Vector3::Zero, GetNmTrajectoryTransform(animHandle) * Matrix::CreateRotationX(-DirectX::XM_PIDIV2));
+	//this->m_position = Vector3::Transform(Vector3::Zero, GetNmTrajectoryTransform(animHandle) * Matrix::CreateRotationX(-DirectX::XM_PIDIV2));
 
 	//We initialise the final transforms to the flver bind pose so we can skip bones unhandled by morpheme in the next loop
 	this->m_boneTransforms = this->m_boneBindPose;
